2650c769f1456396fa4d33a005c1907cd74a5b18
Adnan <99382875+adn80@users.noreply.github.com>
2025-02-26 13:22:28 +0000

    main

---
+++ 04_Datatypes/4_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //Treat all JS code as newer version
+
+// alert(6+6) we are using node.js, not brower
+
+console.log(3 + 3);
+console.log("adnan");
+
+// code readability should be high
+
+let name = "XYZ"
+let age = 19
+let isLoogedIn = false
+let state;
+
+//number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => no value is assigned
+// symbol => unique
+
+// object
+
+console.log(typeof "XYZ");
+console.log(typeof null);
+console.log(typeof undefined);
