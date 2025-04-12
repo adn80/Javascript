eca4a96a15c8a760ca5c53dee11311a46c11b736
Adnan <99382875+adn80@users.noreply.github.com>
2025-04-12 09:51:29 +0000

    conversion of Datatypes

---
+++ 05_conversionOperation/Conversion.js
@@ -0,0 +1,26 @@
+//let score = "adnan"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"35" => 35
+//"35abc" => NaN
+//true => 1; false => 0
+
+let isLoggedIn = "adnan"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1 => true; 0 => false
+// "" => false
+//"adnan" => true
+
+let someNumber = 40
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
